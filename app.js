0985e4c7bfeb30fd63e4c64b7e0296862cb03012
Michael <1992mmoore22@gmail.com>
2023-12-10 13:57:40 -0800

    Removed 404 middleware and root handlers

--- app.js
+++ app.js
@@ -3,43 +3,25 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const express = require("express");
 
-/*----------------- NODE MODULES ------------------*/
-const path = require("path");
-
 /*----------------- CONFIG MODULES ------------------*/
 const corsOptions = require("./cors/corsOptions");
 const logger = require("./logs/logger");
 const redisClient = require("./redisClient");
 
 /*----------------- HANDLER MODULES ------------------*/
-const rootHandlers = require("./handlers/rootHandlers");
 const authHandlers = require("./handlers/authHandlers");
 
 /*----------------- SERVER CONFIGURATIONS ------------------*/
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-/*-------------- CONFIGURATION MIDDLEWARE ---------------*/
+/*-------------- ROOT MIDDLEWARE ---------------*/
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 
-/*---------------- HANDLER MIDDLEWARE -----------------*/
-app.use(rootHandlers);
+/*---------------- PATH MIDDLEWARE -----------------*/
 app.use("/auth", authHandlers);
-
-/*---------------- 404 MIDDLEWARE -----------------*/
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
-});
 
 redisClient
   .connect()
